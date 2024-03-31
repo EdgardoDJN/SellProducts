a201a3cef111a6cf6890521c65f405eb2cf8ad07
Edgardo David <edgardodavid961@gmail.com>
2024-03-31 17:26:59 -0500

    Agregamos el controlador de product

---
+++ src/main/java/com/example/SellProducts/api/ProductController.java
@@ -0,0 +1,61 @@
+package com.example.SellProducts.api;
+
+import com.example.SellProducts.dto.product.ProductDto;
+import com.example.SellProducts.dto.product.ProductToSaveDto;
+import com.example.SellProducts.service.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/products")
+@Validated
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductDto>> getAllProducts() {
+        return ResponseEntity.ok().body(productService.getAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDto> getProductById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok().body(productService.getProductById(id));
+    }
+    @GetMapping("/name")
+    public ResponseEntity<List<ProductDto>> getProductsByName(@RequestParam String name) {
+        return ResponseEntity.ok().body(productService.getProductsByName(name));
+    }
+    @GetMapping("/stock/{quantity}")
+    public ResponseEntity<List<ProductDto>> getProductsByStockGreaterThan(@PathVariable("quantity") int quantity) {
+        return ResponseEntity.ok().body(productService.getProductsByStockGreaterThan(quantity));
+    }
+
+    @GetMapping("/price-stock")
+    public ResponseEntity<List<ProductDto>> getProductsByPriceLessThanAndStockLessThan(@RequestParam double maxPrice, @RequestParam int maxStock) {
+        return ResponseEntity.ok().body(productService.getProductsByPriceLessThanAndStockLessThan(maxPrice, maxStock));
+    }
+    //createProduct
+    @PostMapping
+    public ResponseEntity<ProductDto> createProduct(@Valid @RequestBody ProductToSaveDto productDto) {
+        return ResponseEntity.ok().body(productService.createProduct(productDto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDto> updateProduct(@PathVariable("id") Long id, @RequestBody ProductToSaveDto productDto) {
+        return ResponseEntity.ok().body(productService.updateProduct(id, productDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable("id") Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.ok().build();
+    }
+}
--- src/main/java/com/example/SellProducts/dto/product/ProductToSaveDto.java
+++ src/main/java/com/example/SellProducts/dto/product/ProductToSaveDto.java
@@ -1,11 +1,15 @@
 package com.example.SellProducts.dto.product;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record ProductToSaveDto(
+        @NotNull(message = "Is required")
         String name,
+        @NotNull(message = "Is required")
         Double price,
+        @NotNull(message = "Is required")
         Integer stock
 ) {
     
