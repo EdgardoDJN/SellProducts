f1d681f0ab10edd690b3eae766f918ac39ce44ae
Ts-Pytham <johansanchezdeleon@gmail.com>
2024-03-30 22:46:18 -0500

    Se creó la implementación del servicio de OrderItem

--- src/main/java/com/example/SellProducts/dto/orderItem/OrderItemMapper.java
+++ src/main/java/com/example/SellProducts/dto/orderItem/OrderItemMapper.java
@@ -7,10 +7,10 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface OrderItemMapper {
 
-    @Mapping(target = "id", ignore = true)
-    OrderItemDto toDto(OrderItem orderItem);
-
     @Mapping(target = "orderId", source = "order.id")
     @Mapping(target = "productId", source = "product.id")
+    OrderItemDto toDto(OrderItem orderItem);
+
+    @Mapping(target = "id", ignore = true)
     OrderItem toEntity(CreateOrderItemDto createOrderItemDto);
 }
--- src/main/java/com/example/SellProducts/repositories/OrderItemRepository.java
+++ src/main/java/com/example/SellProducts/repositories/OrderItemRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
     List<OrderItem> findByOrderId(Long orderId);
     List<OrderItem> findByProductName(String productName);
+    List<OrderItem> findByProductId(Long productId);
     @Query("SELECT SUM(oi.price) FROM OrderItem oi WHERE oi.product.id = :productId")
     Double totalSalesAProduct(Long productId);
 }
