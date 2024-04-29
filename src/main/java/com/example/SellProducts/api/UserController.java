83eb2b0b9932283d42d5f49eab5b91efccca4e34
Ts-Pytham <johansanchezdeleon@gmail.com>
2024-04-29 14:17:16 -0500

    Se arregló el código del controller

--- src/main/java/com/example/SellProducts/api/UserController.java
+++ src/main/java/com/example/SellProducts/api/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/products")
+@RequestMapping("api/v1/users")
 public class UserController {
     private final UserService userService;
 
